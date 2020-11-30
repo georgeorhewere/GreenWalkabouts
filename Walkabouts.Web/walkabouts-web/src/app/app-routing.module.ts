0aa48c675b2bb5eb332f356f40a7f96116b1ab63
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-11-30 20:44:34 +0100

    base components and folder architecture

--- Walkabouts.Web/walkabouts-web/src/app/app-routing.module.ts
+++ Walkabouts.Web/walkabouts-web/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+    {
+     path: '',
+     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
+   },
+   {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
