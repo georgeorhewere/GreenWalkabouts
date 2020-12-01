39a7c1a7600845b7727fedd2ec4db5f31b0a9931
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-12-01 13:03:34 +0100

    Added DB context for EF connections

---
+++ Walkabouts.Data/Domain/AppRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Walkabouts.Data.Domain
+{
+    public class AppRole : IdentityRole<long>, IRefEntity
+    {
+        public AppRole()
+        {
+            RefId = Guid.NewGuid();
+        }
+        public Guid RefId { get; set; }
+    }
+}
---
+++ Walkabouts.Repository/Context/WalkaboutsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Walkabouts.Data.Domain;
+
+namespace Walkabouts.Repository.Context
+{
+    public class WalkaboutsDbContext : IdentityDbContext<AppUser, AppRole , long>
+    {
+        public WalkaboutsDbContext(DbContextOptions<WalkaboutsDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //new ProductMap(modelBuilder.Entity<Product>());
+            //new ProductDetailMap(modelBuilder.Entity<ProductDetails>());
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await this.SaveChangesAsync();
+        }
+    }
+}
