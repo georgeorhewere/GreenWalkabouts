b49be0348ee6cecde62bc09c66cfc59ab8b4dd89
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-12-04 07:15:56 +0100

    auth service implementation

---
+++ Walkabouts.Services/Implementations/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Walkabouts.Services.DTO;
+using Walkabouts.Services.Interfaces;
+
+namespace Walkabouts.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+        public LoginUserResultDTO Login(LoginDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RegisterUserResultDTO RegisterUser(RegisterDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
